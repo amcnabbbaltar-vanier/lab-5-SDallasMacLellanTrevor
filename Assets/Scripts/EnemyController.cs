02361f355de85f556aeba3e27e2e9fe66c49e9d9
SDallasMacLellanTrevor <maclellan.trevor@gmail.com>
2025-09-30 22:10:52 -0400

    Finished Lab

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+   private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void GotHit()
+    {
+        animator.SetTrigger("GotHit");
+
+        // Notify GameManager
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.AddScore(1); // Add 1 point per hit
+        }
+    }
+
+}
